b91197b48da547a4ce89a5aa85d85343305498c0
Miles Lee <milesleejob@gmail.com>
2014-06-11 15:58:54 +1000

    Add the List on the Device Panel

--- Js/map.js
+++ Js/map.js
@@ -1,6 +1,10 @@
 'use strict';
 
-angular.module('VM_Mobile2')
-  .controller('MapCtrl', function($scope) {
-	      
+angular.module('VM_Mobile2', ['ngRoute'])
+  .controller('MapCtrl', function($scope, $http) {
+	      $http.get('http://120.151.95.114:8080/GPSServices.svc/GetTrackerDevCrossTable'). //http://rest-service.guides.spring.io/greeting
+              success(function(data) {
+                 $scope.DevInfos = data;
+                 console.log("normal request information:", data);
+              }); 
 });
